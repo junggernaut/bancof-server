5a99e5e06c8183d069b308b0a3f6a46ced927f5c
junggernaut <seungan1419@gmail.com>
2022-10-06 16:49:07 +0900

    fix yarn

--- src/fp/fp.service.ts
+++ src/fp/fp.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { getFetch } from 'src/externalApi/axios';
 import apis from 'src/externalApi/url';
-
 @Injectable()
 export class FpService {
-  getFp(collectionAddress: string) {
+  async getFp(collectionAddress: string) {
     console.log(collectionAddress);
-    // const nftGo = await getFetch(
-    //   `${apis.fp.nftgo.prefix}/${collectionAddress}/metrics`,
-    //   {
-    //     'X-API-KEY': NFTGO_API_KEY,
-    //   },
-    // );
-    // console.log(nftGo);
+    const nftGo = await getFetch(
+      `${apis.fp.nftgo.prefix}/${collectionAddress}/metrics`,
+      {
+        'X-API-KEY': NFTGO_API_KEY,
+      },
+    );
+    console.log(nftGo);
 
     // const looksware = await getFetch(
     //   `${LOOKSWARE_URL_PREFIX}${collectionAddress}`,
